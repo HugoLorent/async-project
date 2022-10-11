ba90d6829ab9742e075891c1e1a6676a994307df
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-10-11 11:01:12 +0200

    finish question 2
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,25 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace async_project
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Runner("Hugo");
+            List<string> runnersName = new List<string>();
+            runnersName.Add("Hugo");
+            runnersName.Add("Rémy");
+            runnersName.Add("Olivier");
+            await Race(runnersName);
         }
 
-        static void Runner(string runnerName)
+        static async Task Runner(string runnerName)
         {
             Random rand = new Random();
+
             int reflexPhase = rand.Next(2, 10) * 100;
-            Task.Delay(reflexPhase).Wait();
+            await Task.Delay(reflexPhase);
             Console.WriteLine($"{runnerName} commence à courir");
 
             int arrivalPhase = rand.Next(8, 16) * 1000;
-            Task.Delay(arrivalPhase).Wait();
+            await Task.Delay(arrivalPhase);
             Console.WriteLine($"{runnerName} est arrivé");
+        }
+
+        static async Task Race(List<string> runnersNames)
+        {
+            Console.WriteLine("Début de la course");
+            Task[] runTasks = new Task[runnersNames.Count];
+
+            for (int i = 0; i < runTasks.Length; i++)
+            {
+                Task runTask = Runner(runnersNames[i]);
+                runTasks[i] = runTask;
+            }
+
+            Task.WaitAll(runTasks);
+
+            Console.WriteLine("La course est terminée");
         }
     }
 }
